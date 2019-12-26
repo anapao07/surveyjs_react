b84954c76b69f37260c503b681cce749566e9ff7
anapao07 <annapao.arcos@gmail.com>
2019-12-26 17:04:11 -0600

    init Survey App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
+app.js
+
+
 import React, { Component } from "react";
 import * as Survey from "survey-react";
-import "survey-react/survey.css";
-import SurveyCreator from "./SurveyCreator";
-import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
 
-import "jquery-ui/themes/base/all.css";
-import "nouislider/distribute/nouislider.css";
-import "select2/dist/css/select2.css";
-import "bootstrap-slider/dist/css/bootstrap-slider.css";
+ import SurveyCreator from "./SurveyCreator";
 
-import "jquery-bar-rating/dist/themes/css-stars.css";
-
-import $ from "jquery";
-import "jquery-ui/ui/widgets/datepicker.js";
-import "select2/dist/js/select2.js";
-import "jquery-bar-rating";
-
-import * as widgets from "surveyjs-widgets";
-
-import "icheck/skins/square/blue.css";
-window["$"] = window["jQuery"] = $;
-require("icheck");
-
-export { MyQuestion } from "./MyQuestion";
-
-Survey.StylesManager.applyTheme("default");
-
-
-widgets.icheck(Survey, $);
-widgets.select2(Survey, $);
-widgets.inputmask(Survey);
-widgets.signaturepad(Survey);
-
-// FIXME: Eliminar los comentarioss
-// Survey.Serializer.addProperty("Prueba",{
-//   "name": "question4",
-//   "type": "picture",
-//   "title": "Fotos de Daño",
-//   "isRequired": true,
-//   "maxPictures": 4,
-//   "minPictures": 1
-// });
-
-
-
+ Survey.StylesManager.applyTheme("darkblue");
 
 class App extends Component {
 
@@ -59,38 +20,140 @@ class App extends Component {
 
 
   render() {
-    // var tempJson = JSON.parse(localStorage.getItem('Prueba')).surveyText;
-    // console.log(tempJson)
-    // var tempJson = JSON.parse(tempJson)
-    // for (const key of Object.keys(tempJson.pages[0].elements)) {
-    //   // console.log(key, tempJson.pages[0].elements[key].type);
-    //   if (tempJson.pages[0].elements[key].type == "file") {
-    //     tempJson.pages[0].elements[key].type = "picture";
-    //   }
-    // }
+  
 
-    // tempJson = JSON.stringify(tempJson)
-    var tempJson = JSON.parse(localStorage.getItem('Prueba'));  
-    var tempJson = JSON.parse(tempJson['surveyText']);
-    for (const key of Object.keys(tempJson.pages[0].elements)) {
-      // console.log(key, tempJson.pages[0].elements[key].type);
-      if (tempJson.pages[0].elements[key].type == "file") {
-        tempJson.pages[0].elements[key].type = "picture";
-      }
-    }
-    console.log(tempJson);
+    let Json = {
+      "pages": [
+       {
+        "name": "página1",
+        "elements": [
+         {
+          "type": "text",
+          "name": "pregunta1",
+          "title": "Carga de Vehículos"
+         },
+         {
+          "type": "boolean",
+          "name": "pregunta2",
+          "title": "¿Todos los vehículos se encuentran listos para viajar?"
+         },
+         {
+          "type": "text",
+          "name": "pregunta3",
+          "visibleIf": "{pregunta2} = false",
+          "title": "Vehículos que no viajan"
+         },
+         {
+          "type": "radiogroup",
+          "name": "pregunta4",
+          "visibleIf": "{pregunta2} = false",
+          "title": "Motivo",
+          "choices": [
+           {
+            "value": "motivo1",
+            "text": "Falta de pedimento"
+           },
+           {
+            "value": "motivo2",
+            "text": "Vehículo faltante"
+           }
+          ]
+         },
+         {
+          "type": "text",
+          "name": "pregunta5",
+          "visibleIf": "{pregunta2} = true",
+          "title": "Hora estimada de salida",
+          "inputType": "time"
+         },
+         {
+          "type": "text",
+          "name": "pregunta6",
+          "visibleIf": "{pregunta2} = true",
+          "title": "Capture la fecha del viaje",
+          "inputType": "date"
+         },
+         {
+          "type": "text",
+          "name": "pregunta7",
+          "title": "Fecha y hora de captura",
+          "inputType": "datetime-local"
+         },
+         {
+          "type": "checkbox",
+          "name": "pregunta8",
+          "title": "Seleccione los instrumentos a utilizar",
+          "choices": [
+           {
+            "value": "item1",
+            "text": "Multimetro"
+           },
+           {
+            "value": "item2",
+            "text": "Osciloscopio"
+           },
+           {
+            "value": "item3",
+            "text": "Pinzas"
+           },
+           {
+            "value": "item4",
+            "text": "Cautín"
+           }
+          ]
+         },
+         {
+          "type": "boolean",
+          "name": "pregunta9",
+          "visibleIf": "{pregunta1} contains \\\"peso\\\"",
+          "title": "¿Desea calcular el peso?"
+         },
+         {
+          "type": "text",
+          "name": "height",
+          "visibleIf": "{pregunta2} = true and {pregunta9} = true",
+          "title": "Estatura",
+          "inputType": "number"
+         },
+         {
+          "type": "text",
+          "name": "shoes",
+          "visibleIf": "{pregunta2} = true and {pregunta9} = true and {pregunta8} = [\"item1\",\"item3\"]",
+          "title": "Altura de zapatos",
+          "inputType": "number"
+         },
+         {
+          "type": "text",
+          "name": "clothes",
+          "visibleIf": "{pregunta2} = true and {pregunta9} = true",
+          "title": "Peso de la ropa",
+          "inputType": "number"
+         },
+         {
+          "type": "text",
+          "name": "weight",
+          "visibleIf": "{pregunta2} = true and {pregunta9} = true",
+          "title": "Peso",
+          "inputType": "number"
+         },
+         {
+          "type": "boolean",
+          "name": "pregunta12",
+          "visibleIf": "(({weight} - {clothes}) / (({height} - {shoes}) * ({height} - {shoes}))) > 24",
+          "title": "Esta seguro que usted pesa mas de 70 KG?"
+         }
+        ]
+       }
+      ]
+     };
+  
 
-
-    var model = new Survey.Model(tempJson);
+    var model = new Survey.Model(Json);
     return (
       <div className="App">
-        <div className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          {/* <h2>Welcome to React with SurveyJS</h2> */}
-        </div>
+      
         <div className="surveyjs">
-          {/*If you want to show survey, uncommehhnt the line below*/}
-          <h1>SurveyJS library in action:</h1>
+         
           <div id="surveyElement"></div>
           <Survey.Survey
 
@@ -98,13 +161,9 @@ class App extends Component {
             onComplete={this.onComplete}
             onValueChanged={this.onValueChanged}
           />
-          {/*If you do not want to show Survey Creator, comment the line below*/}
-          {/* <h1>SurveyJS Creator in action:</h1>
-          <SurveyCreator /> */}
+          
         </div>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
+     
       </div>
     );
   }
